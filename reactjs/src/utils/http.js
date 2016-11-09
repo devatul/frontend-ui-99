8c6032374a41e48e68b4bc21e651dc56595265b5
QuyenHoang664 <Quyenhoang664@gmail.com>
2016-11-09 15:01:52 +0700

    Fix DAT-845

--- reactjs/src/utils/http.js
+++ reactjs/src/utils/http.js
@@ -7,35 +7,35 @@ let count = 0;
 
 function makeRequest({ sync = true, dataType = 'json', contentType = "application/json", method = 'GET', path, params = {}, success, error }) {
 
-       return $.ajax({
-				url: Constants.SERVER_API + path,
-				dataType: dataType,
-				contentType: contentType,
-                async: sync,
-				type: method,
-				data: params,
-				beforeSend: function(xhr) {
-					xhr.setRequestHeader("Authorization", "JWT " + sessionStorage.getItem('token'));
-				},
-				success: function(data) {
-					success &&
-                        success(data);
-				}.bind(this),
-				error: function(err) {
-					switch(true) {
-						case (err.status === 401):
-							browserHistory.push('/Account/SignIn');
-						break;
-						case (err.status === 500 || err.status === 503 || err.textStatus === 'timeout'):
-							if(count < limitRetry) {
-								makeRequest({ sync, dataType, contentType, method, path, params, success, error });
-								count++;
-							}
-					}
-					error &&
-						error(err)
-				}.bind(this)
-			});
+   return $.ajax({
+			url: Constants.SERVER_API + path,
+			dataType: dataType,
+			contentType: contentType,
+            async: sync,
+			type: method,
+			data: params,
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Authorization", "JWT " + sessionStorage.getItem('token'));
+			},
+			success: function(data) {
+				success &&
+                    success(data);
+			}.bind(this),
+			error: function(err) {
+				switch(true) {
+					case (err.status === 401):
+						browserHistory.push('/Account/SignIn');
+					break;
+					case (err.status === 500 || err.status === 503 || err.textStatus === 'timeout'):
+						if(count < limitRetry) {
+							makeRequest({ sync, dataType, contentType, method, path, params, success, error });
+							count++;
+						}
+				}
+				error &&
+					error(err)
+			}.bind(this)
+		});
 }
 
 
