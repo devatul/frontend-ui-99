93b3f7c4e9163afd3f4f3b49626ec2cb2210846f
Tung Son <Tung Son>
2016-11-10 19:56:02 +0700

    fix DAT 863 , message Constant.js , validateInput component

--- reactjs/src/Insight/Identity.js
+++ reactjs/src/Insight/Identity.js
@@ -1,253 +1,254 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-import {Link, IndexLink, browserHistory} from 'react-router'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { Link, IndexLink, browserHistory } from 'react-router'
 import template from './Identity.rt'
-import Constant, {fetching} from '../Constant.js'
+import Constant, { fetching } from '../Constant.js'
 import javascript from '../script/javascript.js'
 import update from 'react/lib/update'
 import _ from 'lodash'
-import $, {JQuery} from 'jquery'
-import {makeRequest} from '../utils/http'
+import $, { JQuery } from 'jquery'
+import { makeRequest } from '../utils/http'
 
 let Indentity = React.createClass({
-  getInitialState() {
-    return {
-      numberUser: 5,
-      data_exports: {},
-      sizeFilter: 0,
-      height_0: 0,
-      height_1: 0,
-      height_2: 0,
-      height_3: 0,
-      save_dataChart: {},
-      save_cvs: {},
-      scan_result: {},
-      rickInsight: {},
-      dataChart: {
-        high_risk_users: {},
-        high_risk_directory: {},
-        key_contributor: []
-      },
-      display: 0,
+    getInitialState() {
+        return {
+            numberUser: 5,
+            data_exports: {},
+            sizeFilter: 0,
+            height_0: 0,
+            height_1: 0,
+            height_2: 0,
+            height_3: 0,
+            save_dataChart: {},
+            save_cvs: {},
+            scan_result: {},
+            rickInsight: {},
+            dataChart: {
+                high_risk_users: {},
+                high_risk_directory: {},
+                key_contributor: []
+            },
+            display: 0,
 
-      xhr: {
-        status: "Report is loading",
-        message: "Please wait!",
-        timer: 20,
-        loading: 0,
-        isFetching: fetching.STARTED
-      },
-    };
-  },
+            xhr: {
+                status: "Report is loading",
+                message: "Please wait!",
+                timer: 20,
+                loading: 0,
+                isFetching: fetching.STARTED
+            },
+        };
+    },
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return (this.state.dataChart != nextState.dataChart) || (this.setState.sizeFilter != nextState.sizeFilter) || (this.state.data_exports != nextState.data_exports)
-  },
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.state.dataChart != nextState.dataChart) || (this.setState.sizeFilter != nextState.sizeFilter) || (this.state.data_exports != nextState.data_exports)
+    },
 
-  handleFilter(bodyRequest) {
-    if (!_.isNull(bodyRequest)) {
-      let value = bodyRequest.number_users;
+    handleFilter(bodyRequest) {
+        if (!_.isNull(bodyRequest)) {
+            let value = bodyRequest.number_users;
 
-      if (value == 'Top 5') {
-        value = 5
-      }
-      if (value == 'Top 15') {
-        value = 15
-      }
-      if (value == 'Top 25') {
-        value = 25
-      }
-      if (value == 'Top 50') {
-        value = 50
-      }
+            if (value == 'Top 5') {
+                value = 5
+            }
+            if (value == 'Top 15') {
+                value = 15
+            }
+            if (value == 'Top 25') {
+                value = 25
+            }
+            if (value == 'Top 50') {
+                value = 50
+            }
 
-      let call = makeRequest({
-        path: 'api/insight/iam?number_users=' + value,
-        success: (data) => {
-          this.updateChartData(data)
+            let call = makeRequest({
+                path: 'api/insight/iam?number_users=' + value,
+                success: (data) => {
+                    this.updateChartData(data)
+                }
+            });
         }
-      });
-    }
-  },
+    },
 
-  getData() {
-    return makeRequest({
-      path: 'api/insight/iam?number_users=5',
-      success: (data) => {
-        this.updateChartData(data);
-      }
-    });
-  },
+    getData() {
+        return makeRequest({
+            path: 'api/insight/iam?number_users=5',
+            success: (data) => {
+                this.updateChartData(data);
+            }
+        });
+    },
 
-  updateChartData(datas) {
-    let high_risk_users = {},
-        high_risk_directory = {},
-        arr = [],
-        key_contributor = [],
-        height_0 = 0,
-        height_1 = 0,
-        height_2 = 0,
-        height_3 = 0;
+    updateChartData(datas) {
+        let high_risk_users = {},
+            high_risk_directory = {},
+            arr = [],
+            key_contributor = [],
+            height_0 = 0,
+            height_1 = 0,
+            height_2 = 0,
+            height_3 = 0;
 
-    high_risk_users = this.configChart(datas.high_risk_users);
-    high_risk_directory = this.configChart(datas.high_risk_directory);
+        high_risk_users = this.configChart(datas.high_risk_users);
+        high_risk_directory = this.configChart(datas.high_risk_directory);
 
-    for (let i = 0; i < _.size(datas.key_contributor); i++) {
-      arr[i] = datas.key_contributor[i];
-      key_contributor.push({
-        category_name: arr[i].category_name,
-        contributors: (this.configChart(arr[i].contributors))
-      })
-    }
-
-    let length = key_contributor.length;
-
-
-    /* begin : config hight chart*/
-    height_0 = _.size(high_risk_users) > _.size(high_risk_directory) ? _.size(high_risk_users) * 100 : _.size(high_risk_directory) * 100;
-
-    if(length == 1) {
-      height_1 = _.size(key_contributor[0].contributors.categories)* 40;
-    } else{
-      height_1 = Math.max(_.size(key_contributor[0].contributors.categories), _.size(key_contributor[1].contributors.categories)) * 40;
-
-      if(length == 3) {
-        height_2 = _.size(key_contributor[2].contributors.categories)* 40;
-      } else if(length == 4) {
-        height_2 = Math.max(_.size(key_contributor[2].contributors.categories), _.size(key_contributor[3].contributors.categories)) * 40;
-      } else if( length == 5) {
-        height_2 = Math.max(_.size(key_contributor[2].contributors.categories), _.size(key_contributor[3].contributors.categories)) * 40;
-        height_3 = _.size(key_contributor[5].contributors.categories)* 40;
-      } else if( length == 6) {
-        height_2 = Math.max(_.size(key_contributor[2].contributors.categories), _.size(key_contributor[3].contributors.categories)) * 40;
-        height_3 = Math.max(_.size(key_contributor[4].contributors.categories), _.size(key_contributor[5].contributors.categories)) * 40;
-      }
-    }
-    /*end*/
-
-    let updateData_config = update(this.state, {
-      dataChart: {
-        high_risk_users: {$set: high_risk_users},
-        high_risk_directory: {$set: high_risk_directory},
-        key_contributor: {$set: key_contributor}
-      },
-      save_dataChart: {
-        high_risk_users: {$set: high_risk_users},
-        high_risk_directory: {$set: high_risk_directory},
-        key_contributor: {$set: key_contributor}
-      },
-      data_exports: {$set: datas},
-      save_cvs: {$set: datas},
-      height_0: {$set: height_0},
-      height_1: {$set: height_1},
-      height_2: {$set: height_2},
-      height_3: {$set: height_3},
-      display: {$set: length},
-
-      xhr: {
-        isFetching: {
-          $set: fetching.SUCCESS
+        for (let i = 0; i < _.size(datas.key_contributor); i++) {
+            arr[i] = datas.key_contributor[i];
+            key_contributor.push({
+                category_name: arr[i].category_name,
+                contributors: (this.configChart(arr[i].contributors))
+            })
         }
-      }
-    });
 
-    this.setState(updateData_config)
+        let length = key_contributor.length;
 
-  },
 
-  configChart(object) {
-    let colors = ['#5bc0de', '#349da2', '#7986cb', '#ed9c28', '#E36159', '#edc240', '#8cc1d1', '#b0d6e1', '#349da1', '#8ababc', '#aecccc', '#7986cc', '#a5aaca', '#c0c4df', '#e46159'],
-        dataChart = [],
-        categories = [];
+        /* begin : config hight chart*/
+        height_0 = _.size(high_risk_users) > _.size(high_risk_directory) ? _.size(high_risk_users) * 100 : _.size(high_risk_directory) * 100;
+
+        if (length == 1) {
+            height_1 = _.size(key_contributor[0].contributors.categories) * 40;
+        } else {
+            height_1 = Math.max(_.size(key_contributor[0].contributors.categories), _.size(key_contributor[1].contributors.categories)) * 40;
+
+            if (length == 3) {
+                height_2 = _.size(key_contributor[2].contributors.categories) * 40;
+            } else if (length == 4) {
+                height_2 = Math.max(_.size(key_contributor[2].contributors.categories), _.size(key_contributor[3].contributors.categories)) * 40;
+            } else if (length == 5) {
+                height_2 = Math.max(_.size(key_contributor[2].contributors.categories), _.size(key_contributor[3].contributors.categories)) * 40;
+                height_3 = _.size(key_contributor[5].contributors.categories) * 40;
+            } else if (length == 6) {
+                height_2 = Math.max(_.size(key_contributor[2].contributors.categories), _.size(key_contributor[3].contributors.categories)) * 40;
+                height_3 = Math.max(_.size(key_contributor[4].contributors.categories), _.size(key_contributor[5].contributors.categories)) * 40;
+            }
+        }
+        /*end*/
+
+        let updateData_config = update(this.state, {
+            dataChart: {
+                high_risk_users: { $set: high_risk_users },
+                high_risk_directory: { $set: high_risk_directory },
+                key_contributor: { $set: key_contributor }
+            },
+            save_dataChart: {
+                high_risk_users: { $set: high_risk_users },
+                high_risk_directory: { $set: high_risk_directory },
+                key_contributor: { $set: key_contributor }
+            },
+            data_exports: { $set: datas },
+            save_cvs: { $set: datas },
+            height_0: { $set: height_0 },
+            height_1: { $set: height_1 },
+            height_2: { $set: height_2 },
+            height_3: { $set: height_3 },
+            display: { $set: length },
+
+            xhr: {
+                isFetching: {
+                    $set: fetching.SUCCESS
+                }
+            }
+        });
+
+        this.setState(updateData_config)
+
+    },
+
+    configChart(object) {
+        let colors = ['#5bc0de', '#349da2', '#7986cb', '#ed9c28', '#E36159', '#edc240', '#8cc1d1', '#b0d6e1', '#349da1', '#8ababc', '#aecccc', '#7986cc', '#a5aaca', '#c0c4df', '#e46159'],
+            dataChart = [],
+            categories = [];
         object = _.orderBy(object, ['docs'], ['desc']);
 
-    for (let i = 0; i < _.size(object); i++) {
-      categories.push(object[i].name);
-      dataChart.push({
-        y: object[i].docs,
-        color: colors[i],
-      })
-    }
+        for (let i = 0; i < _.size(object); i++) {
+            categories.push(object[i].name);
+            dataChart.push({
+                y: object[i].docs,
+                color: colors[i],
+            })
+        }
 
-    return {
-      categories: categories,
-      data: dataChart
-    }
-  },
+        return {
+            categories: categories,
+            data: dataChart
+        }
+    },
 
-  upperFirst(value) {
-    let sp = _.split(value, ' '), rt = '';
+    upperFirst(value) {
+        let sp = _.split(value, ' '),
+            rt = '';
 
-    for (let i = 0; i < sp.length; i++) {
-      rt += _.upperFirst(sp[i]) + ' ';
-    }
+        for (let i = 0; i < sp.length; i++) {
+            rt += _.upperFirst(sp[i]) + ' ';
+        }
 
-    return rt;
-  },
+        return rt;
+    },
 
-  convertArrayOfObjectsToCSV(args) {
-    let result, ctr, keys, columnDelimiter, lineDelimiter, data;
+    convertArrayOfObjectsToCSV(args) {
+        let result, ctr, keys, columnDelimiter, lineDelimiter, data;
 
-    data = args.data || null;
-    if (data == null || !data.length) {
-      return null;
-    }
+        data = args.data || null;
+        if (data == null || !data.length) {
+            return null;
+        }
 
-    columnDelimiter = args.columnDelimiter || ',';
-    lineDelimiter = args.lineDelimiter || '\n';
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
 
-    keys = Object.keys(data[0]);
+        keys = Object.keys(data[0]);
 
-    result = '';
-    result += keys.join(columnDelimiter);
-    result += lineDelimiter;
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
 
-    data.forEach(function (item) {
-      ctr = 0;
-      keys.forEach(function (key) {
-        if (ctr > 0) result += columnDelimiter;
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
 
-        result += item[key];
-        ctr++;
-      });
-      result += lineDelimiter;
-    });
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
 
-    return result;
-  },
+        return result;
+    },
 
-  downloadCSV(value, datas) {
-    let data, filename, link,
-      csv = this.convertArrayOfObjectsToCSV({
-        data: datas
-      });
+    downloadCSV(value, datas) {
+        let data, filename, link,
+            csv = this.convertArrayOfObjectsToCSV({
+                data: datas
+            });
 
-    if (csv == null) return;
+        if (csv == null) return;
 
-    filename = value + '.csv' || 'export.csv';
+        filename = value + '.csv' || 'export.csv';
 
-    if (!csv.match(/^data:text\/csv/i)) {
-      csv = 'data:text/csv;charset=utf-8,' + csv;
-    }
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
 
-    data = encodeURI(csv);
+        data = encodeURI(csv);
 
-    link = document.createElement('a');
-    link.setAttribute('href', data);
-    link.setAttribute('download', filename);
-    link.click();
-  },
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+    },
 
-  formatNameCategory: (str) => _.replace(str, '/', ' / '),
+    formatNameCategory: (str) => _.replace(str, '/', ' / '),
 
-  formatNumber: (num) => num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"),
+    formatNumber: (num) => num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"),
 
-  componentDidMount(prevProps, prevState) {
-    this.getData(prevProps, prevState);
-    javascript();
-  },
+    componentDidMount(prevProps, prevState) {
+        this.getData(prevProps, prevState);
+        javascript();
+    },
 
-  render: template
+    render: template
 });
 
 module.exports = Indentity;
---
+++ reactjs/src/components/dathena/ValidateInput.js
@@ -0,0 +1,146 @@
+'Use Strict';
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
+import _ from 'lodash'
+import Constant from '../../Constant.js';
+
+
+var Input = React.createClass({
+    /*PropsType :{
+        type : PropsType.string,
+    },
+    */
+    getInitialState() {
+        return {
+            check: this.props.check,
+            error: ''
+        }
+    },
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.props.value != nextProps.value) {
+            return true
+        }
+        if (this.state.error != nextState.error) {
+            return true
+        }
+        if (this.props.check !=  nextProps.check) {
+            return true
+        }
+        if (this.props.data !=  nextProps.data) {
+            return true
+        }
+        return false
+    },
+
+    componentDidUpdate(prevProps , prevState){
+         if (this.props.check !=  prevProps.check) {
+             this.validate(this.props.data)
+        }
+    },
+
+    validate(event) {
+        debugger
+        if(event == undefined) {
+            this.setState({error : 'aaaa'}) ;
+            return
+        }
+        let value = event.target.value == undefined ? event : event.target.value,
+            { validate , name , minlength} = this.props,
+            message = '',
+            listValidate = _.split(validate , ',');
+            if (!_.isNull(validate)) {
+                for (let i = 0; i < listValidate.length; i++) {
+                    switch (listValidate[i].trim()) {
+                        case 'required':
+                           message =  this.checkedNull(value , name);
+                            break;
+                        case 'email':
+                           message =  this.checkEmail(value , name);
+                            break;
+                        case 'number':
+                           message =  this.checkNumber(value , name);
+                            break;
+                    }
+                };
+                if(minlength != undefined || minlength != null) {
+                    message =  minlength > value.length ? this.getMessage(name).minlength : '';
+                    this.setState({ error: message })
+                }
+                 this.props.onChange(event , message)
+
+            }
+    },
+   /* onChange(event) {
+        let value = _.trim(event.target.value);
+        if (value == "") {
+            console.log("value_null :", "yes")
+        }
+        switch (this.props.name) {
+            case 'number':
+                this.checkNumber(value);
+                break;
+            case "email":
+                this.checkEmail(value);
+                break;
+        }
+    },*/
+    getMessage(name){
+        let listMess  = Constant.VALIDATION ;
+        for(let i = 0 ; i < _.size(listMess) ; i++){
+            if(Object.keys(listMess)[i] == name) {
+                return Object.values(listMess)[i]
+            }
+        }
+    },
+    checkedNull(value , name){
+        debugger
+        let message = value == "" ? this.getMessage(name).required : '';
+        this.setState({ error: message })
+        return message
+    },
+
+    checkNumber(value , name) {
+
+        let message = isNaN(value) ? this.getMessage(name).number : '';
+        this.setState({ error: message })
+        return message
+    },
+    checkEmail(value , name) {
+        let filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/,
+            message = !filter.test(value) ? this.getMessage(name).required : '' ;
+        this.setState({ error: message })
+        return message
+    },
+    onChange(event) {
+        debugger
+        let value = event.target.value
+        this.validate(event);
+
+    },
+    render() {
+
+        let disabled = this.props.disabled;
+        let className = this.state.error != '' ? this.props.className  + ' has-error' : this.props.className  ;
+        return (
+            < div key = { this.props.key } >
+                < input type = { this.props.type }
+                    className =  {className}
+                    id = { this.props.id }
+                    placeholder = { this.props.placeholder }
+                    name = { this.props.name }
+                    value = { this.props.value }
+                    checked = { this.props.checked }
+                    disabled = { disabled }
+                    onChange = { this.onChange }
+                    onBlur = { this.validate }
+                />
+                <span className="error">{this.state.error}</span>
+
+
+            </div >
+
+        )
+    }
+});
+
+module.exports = Input
