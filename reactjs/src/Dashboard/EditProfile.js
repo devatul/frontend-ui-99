7056cfad7a1eb81cf0f19e7c4b3d0ff766a2cefa
Tung Son <Tung Son>
2016-11-11 17:37:09 +0700

    clean code EditProfile.js

--- reactjs/src/Dashboard/EditProfile.js
+++ reactjs/src/Dashboard/EditProfile.js
@@ -18,79 +18,25 @@ module.exports = React.createClass({
 
         };
     },
-    changePhoto: function() {
-        $.ajax({
-            url: Constant.SERVER_API + 'api/account/change_photo/',
-            dataType: 'json',
-            type: 'PUT',
-            data: {
-
-            },
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "JWT " + sessionStorage.getItem('token'));
-            },
-            success: function(data) {
-                console.log("ChangePass: ", data);
-            }.bind(this),
-            error: function(xhr, status, error) {
-                console.log(xhr);
-                var jsonResponse = JSON.parse(xhr.responseText);
-                console.log(jsonResponse);
-                if (xhr.status === 401) {
-                    browserHistory.push('/Account/SignIn');
-                }
-            }.bind(this)
+    getProfile(){
+         return makeRequest({
+            path: 'api/account/profile/',
+            success: (data) => {
+                this.setState({ profile: data })
+            }
         });
     },
-
-    componentWillMount() {
-        $.ajax({
-            url: Constant.SERVER_API + 'api/account/profile/',
-            dataType: 'json',
-            type: 'GET',
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "JWT " + sessionStorage.getItem('token'));
-            },
-            success: function(data) {
-
-                this.setState({ profile: data });
-
-                console.log("scan result: ", data);
-            }.bind(this),
-            error: function(xhr, status, error) {
-                console.log(xhr);
-                var jsonResponse = JSON.parse(xhr.responseText);
-                console.log(jsonResponse);
-                if (xhr.status === 401) {
-                    browserHistory.push('/Account/SignIn');
-                }
-            }.bind(this)
-        });
-        $.ajax({
-            url: Constant.SERVER_API + 'api/account/change_photo/',
-            dataType: 'json',
-            type: 'GET',
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "JWT " + sessionStorage.getItem('token'));
-            },
-            success: function(data) {
-
-                this.setState({ photo: data });
-
-                console.log("photo: ", photo);
-            }.bind(this),
-            error: function(xhr, status, error) {
-                console.log(xhr);
-                var jsonResponse = JSON.parse(xhr.responseText);
-                console.log(jsonResponse);
-                if (xhr.status === 401) {
-                    browserHistory.push('/Account/SignIn');
-                }
-            }.bind(this)
+    getPhoto(){
+        return makeRequest({
+            path: 'api/account/change_photo/',
+            success: (data) => {
+                this.setState({ photo: data })
+            }
         });
     },
-
     componentDidMount() {
+        this.getProfile() ,
+        this.getPhoto()
     },
     render: template
 });
