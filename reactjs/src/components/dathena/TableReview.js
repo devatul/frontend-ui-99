129fccd9e1d664de3d203ab46ab59268eb213796
Jeremie Simon <jeremie.simon@dathena.io>
2016-11-21 18:47:27 +0800

    misc: fix typos

--- reactjs/src/components/dathena/TableReview.js
+++ reactjs/src/components/dathena/TableReview.js
@@ -696,7 +696,7 @@ var Row = React.createClass({
               <li>Creation Date: <b>{document.creation_date}</b></li>
               <li>Modification Date: <b>{document.modification_date}</b></li>
               {document.legal_retention_until ? <li>Required Legal Retention until: <b>{document.legal_retention_until}</b></li> : ''}
-              <li>Confidentiality Level: <b> yes/no</b></li>
+              <li>Confidentiality Level: <b> N/A</b></li>
               <li>Number of Classification Challenge: <b>{document.number_of_classification_challenge}</b></li>
             </InfoButton>
           </td>
